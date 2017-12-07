421a664d8d04083a5751392a692e417404d326f2
xiaosophiameng <xiaosophiameng@gmail.com>
2017-12-07 00:02:41 -0800

    update get_vix_to_local.py file by adding arguments and path

--- src/get_vix_to_local.py
+++ src/get_vix_to_local.py
@@ -6,14 +6,18 @@
 # libraries/packages used
 import urllib.request
 import os
+import sys
+
+url = sys.argv[1]
+dst = sys.argv[2]
 
 #main function
-url = 'http://www.cboe.com/publish/scheduledtask/mktdata/datahouse/vixcurrent.csv'
+#url = 'http://www.cboe.com/publish/scheduledtask/mktdata/datahouse/vixcurrent.csv'
 response = urllib.request.urlopen(url)
 data = response.read().decode('UTF-8')
 data = '\n'.join(data.split('\n')[1:])
 
 #output file
-output_file_directory = os.path.join(os.pardir, 'data', 'vix_data.csv')
+output_file_directory = os.path.join(os.pardir, dst)
 with open(output_file_directory,'w') as output:
     output.write(data)
