1a7836103c3dac9ab55c8413fd94ca437a4fb763
xiaosophiameng <xiaosophiameng@gmail.com>
2017-12-09 01:02:50 -0800

    update plot_vix_analysis.py file from using sys to using argparse

--- src/plot_vix_analysis.py
+++ src/plot_vix_analysis.py
@@ -4,22 +4,32 @@ import csv
 import numpy
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
-import sys
-src = sys.argv[1]
-dst = sys.argv[2]
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('src')
+parser.add_argument('dst')
+args = parser.parse_args()
 
-interval = []
-corrcoef = []
-with open(os.path.join(os.pardir, src)) as csvfile:
-    readCSV = csv.reader(csvfile, delimiter=',')
-    for row in readCSV:
-        interval.append(row[0])
-        corrcoef.append(row[1])
-interval = numpy.array([float(i) for i in interval[1:]])
-corrcoef = numpy.array([float(c) for c in corrcoef[1:]])
+#main function
+def main():
+    src = args.src
+    dst = args.dst
+    interval = []
+    corrcoef = []
+    with open(os.path.join(os.pardir, src)) as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=',')
+        for row in readCSV:
+            interval.append(row[0])
+            corrcoef.append(row[1])
+    interval = numpy.array([float(i) for i in interval[1:]])
+    corrcoef = numpy.array([float(c) for c in corrcoef[1:]])
 
-fig = plt.figure()
-plt.plot(interval, corrcoef, 'ro')
-pp = PdfPages(os.path.join(os.pardir, dst) + '.pdf')
-pp.savefig(fig)
-pp.close()
+    fig = plt.figure()
+    plt.plot(interval, corrcoef, 'ro')
+    pp = PdfPages(os.path.join(os.pardir, dst) + '.pdf')
+    pp.savefig(fig)
+    pp.close()
+
+# call main function
+if __name__ == "__main__":
+    main()
