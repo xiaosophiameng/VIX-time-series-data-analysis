3ca9f9f965c64f1057ec82a7e147d8aa45723e63
xiaosophiameng <xiaosophiameng@gmail.com>
2017-12-12 15:50:51 -0800

    update get_vix_to_local.py file to make the path inside work for makefile

--- src/get_vix_to_local.py
+++ src/get_vix_to_local.py
@@ -10,22 +10,22 @@ import urllib.request
 import os
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('url')
+#parser.add_argument('url')
 parser.add_argument('dst')
 args = parser.parse_args()
 
 #main function
 def main():
-    url = args.url
+    #url = args.url
     dst = args.dst
 
     #input file
-    response = urllib.request.urlopen(url)
+    response = urllib.request.urlopen("http://www.cboe.com/publish/scheduledtask/mktdata/datahouse/vixcurrent.csv")
     data = response.read().decode('UTF-8')
     data = '\n'.join(data.split('\n')[1:])
 
     #output file
-    output_file_directory = os.path.join(os.pardir, dst)
+    output_file_directory = os.path.join(dst)
     with open(output_file_directory,'w') as output:
         output.write(data)
 
